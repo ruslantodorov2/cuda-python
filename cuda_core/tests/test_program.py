c5afc59dc631e0de929eac67e4e37868ff5348cb
ksimpson <ksimpson@nvidia.com>
2024-11-12 16:38:22 -0800

    fix the dataclass so that only explixitely defined options are passed

--- cuda_core/tests/test_program.py
+++ cuda_core/tests/test_program.py
@@ -6,10 +6,41 @@
 # this software and related documentation outside the terms of the EULA
 # is strictly prohibited.
 
-from cuda.core.experimental._program import Program
+from cuda.core.experimental._program import Program, ProgramOptions
 from cuda.core.experimental._module import ObjectCode, Kernel
 from cuda.core.experimental._device import Device
 import pytest
+
+def test_program_with_various_options(init_cuda):
+    code = "extern \"C\" __global__ void my_kernel() {}"
+    
+    options_list = [
+        ProgramOptions(device_w=True, dopt=True, ptxas_options="-v"),
+        ProgramOptions(relocatable_device_code=True, maxrregcount=32),
+        ProgramOptions(ftz=True, prec_sqrt=False, prec_div=False),
+        ProgramOptions(fmad=False, use_fast_math=True),
+        ProgramOptions(extra_device_vectorization=True, modify_stack_limit=False),
+        ProgramOptions(dlink_time_opt=True, gen_opt_lto=True),
+        ProgramOptions(optix_ir=True, jump_table_density=50),
+        ProgramOptions(device_stack_protector=True, define_macro="MY_MACRO"),
+        ProgramOptions(undefine_macro="MY_MACRO", include_path="/usr/local/include"),
+        ProgramOptions(pre_include="my_header.h", no_source_include=True),
+        ProgramOptions(builtin_initializer_list=False, disable_warnings=True),
+        ProgramOptions(restrict=True, device_as_default_execution_space=True),
+        ProgramOptions(device_int128=True, optimization_info="inline"),
+        ProgramOptions(display_error_number=False, no_display_error_number=True),
+        ProgramOptions(diag_error="1234", diag_suppress="5678"),
+        ProgramOptions(diag_warn="91011", brief_diagnostics=True),
+        ProgramOptions(time="compile_time.csv", split_compile=4),
+        ProgramOptions(fdevice_syntax_only=True, minimal=True)
+    ]
+    
+    #TODO compile the program once the CI is set up
+    for options in options_list:
+        program = Program(code, "c++", options)
+        assert program.backend == "nvrtc"
+        program.close()
+        assert program.handle is None
 
 def test_program_init_valid_code_type():
     code = "extern \"C\" __global__ void my_kernel() {}"
